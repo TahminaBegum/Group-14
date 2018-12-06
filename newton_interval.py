88cdf461aae43f7e897b4ad4d3ecedc676f5e813
DemetDemirkiran <35539445+DemetDemirkiran@users.noreply.github.com>
2018-12-06 10:22:02 +0100

    Update newton_interval.py

--- newton_interval.py
+++ newton_interval.py
@@ -64,25 +64,113 @@ def mean_of_interval(qx, new_qx):
 def interval_newton_method(f, qx, new_qx , Ep):
 
     if(qx < new_qx):
-        lower = FloatDPBounds(qx)
-        higher = FloatDPBounds(new_qx)
+        lower = qx
+        higher = new_qx
         #print(lower, higher)
     elif(qx > new_qx):   
-        lower = FloatDPBounds(new_qx)
-        higher = FloatDPBounds(qx)
+        lower = new_qx
+        higher = qx
         #print(lower, higher)
     #mean_of_interval(lower, higher) != 0.01
+    
+    #IX=UpperInterval({cast_exact(lower):cast_exact(higher)})
+    #X = cast_singleton(IX)
+
+    #mx = X.value()
+    #N = mx - ( f( mx ) / derivative( f, X ) )
+
     i=0
-    delta = derivative(f, qx) 
+    #delta = derivative(f, qx) 
+    print("lower",lower ,"higher",higher)
     mx = mean_of_interval(lower, higher)
     N_lower = mx - ( f( mx ) / derivative( f, lower ) )
     N_higher = mx - ( f( mx ) / derivative( f, higher ) )
-    
-    #while(delta!=0):
-       # i=i+1
-       # interval_newton_method(f, N_lower, N_higher , Ep)
+    print("Nlower",N_lower ,"Nhigher",N_higher)
+
+    while(i!=5):
+        mx = mean_of_interval(lower, higher)
+        N_lower = mx - ( f( mx ) / derivative( f, lower ) )
+        N_higher = mx - ( f( mx ) / derivative( f, higher ) )
+        print(lower, higher)
+        print(N_lower, N_higher)
+        if((N_lower > lower) and (N_higher < higher)):
+            mx = mean_of_interval(lower, N_higher)
+            #lower = mx - ( f( mx ) / derivative( f, lower ) )
+            higher = mx - ( f( mx ) / derivative( f, N_higher ) )
+            #higher = N_higher
+            print("New bounds(1)", lower, N_higher)
+            lower = mx - ( f( mx ) / derivative( f, lower ) )
+            i=i+1
+        elif((lower < N_lower) and (N_higher < higher)):
+            mx = mean_of_interval(N_lower, N_higher)
+            N_lower = mx - ( f( mx ) / derivative( f, N_lower ) )
+            print("New bounds(2)", N_lower, N_higher)
+            i=i+1
+        elif((N_lower > lower) and (N_higher > higher)):
+            mx = mean_of_interval(lower, higher)
+            N_lower = mx - ( f( mx ) / derivative( f, lower ) ) 
+            N_higher = mx - ( f( mx ) / derivative( f, higher ) )
+            print("New bounds(3)", lower, higher)
+            i=i+1
+        elif((lower < N_lower) and (N_higher >= higher)):
+            mx = mean_of_interval(N_lower, higher)
+            N_higher = mx - ( f( mx ) / derivative( f, N_higher ) )
+            print("New bounds(4)", N_lower, higher)
+            i=i+1
+        #if(decide(abs(delta)<=Ep)):
+            #break
+    print("Root in interval", lower, higher )   
 
 
+
+
+
+
+
+
+
+
+
+
+    #if((N_lower > lower)):
+
+       # if((N_higher > higher)):
+          #  N_lower = lower
+          #  higher = higher
+          #  print(2)
+       # if((N_higher < higher)):
+         #   N_lower = lower
+         #   higher = N_higher
+        #    print(1)
+    #elif((N_higher > higher)):
+        #if((N_lower > lower)):
+           # N_lower = lower
+            #N_higher = higher
+            #print(3)
+        #if((N_higher < higher)):
+           # N_lower = lower
+            #higher = N_higher
+           # print(4)
+       
+
+    #print(lower, higher)
+    #if (refines(N_lower,lower) and refines( N_higher, higher)):
+        #print("Verified root in",N_lower,"within",lower)
+    #elif (inconsistent(N_lower,lower) and inconsistent(N_higher, higher)):
+        #print("No root in",lower, N_higher)
+    #else:
+            #print("Any root in",lower,"also lies in",N_lower,"so in common refinement",refinement(lower,N_lower))
+
+    #for (nl,nh) in range(N_lower, N_higher):
+        #mx = mean_of_interval(N_lower, N_higher)
+        #nl = mx - ( f( mx ) / derivative( fterval, N_lower ) )
+        #nh = mx - ( f( mx ) / derivative( f, N_higher ) )
+        #if((nl < N_lower) and (nh < N_higher)):
+            #N_higher = nh
+            #print("in for in",N_lower, n_higher)
+            #print(IX)
+            #break
+        #break
 
    # if((N_lower > lower) and (N_higher < higher )):
   #      IX=UpperInterval({cast_exact(N_lower):cast_exact(N_higher)})
@@ -194,23 +282,23 @@ def main():
     if choice == 1:
         random_newton_step(f)
         qx, new_qx = random_newton_step(f)
-        contractor(f, qx, new_qx, Ep)
+        interval_newton_method(f, qx, new_qx, Ep)
 
     if choice == 2:
         initialization_number = int(input("Enter the first interval:"))
         #print(initialization_number)
        # q = cast_singleton(initialization_number)
-        qx = FloatDPBounds(initialization_number)
+        qx = FloatMPBounds(initialization_number)
         newton_step(f, qx)
         new_qx = newton_step(f, qx)
-        contractor(f, qx, new_qx, Ep)
+        interval_newton_method(f, qx, new_qx, Ep)
 
     if choice == 3:
         first = int(input("Enter the first interval:"))
         second = int(input("Enter the second interval:")) 
-        fx = FloatDPBounds(first)
-        sx = FloatDPBounds(second)
-        contractor(f, fx,sx, Ep)
+        fx = FloatMPBounds(first)
+        sx = FloatMPBounds(second)
+        interval_newton_method(f, fx,sx, Ep)
        
     
 
