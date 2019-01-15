a407827f503bf2494222ee690d678a30f98ee748
Martyna <mikos.martyna@gmail.com>
2019-01-15 15:20:52 +0100

    epsilon fix/graph fix

--- nm11.py
+++ nm11.py
@@ -16,9 +16,12 @@ def print_function():
  
     print("1. x*x-2")
     print("2. x*x*x-2")
-    print("3. (-x*x)-2*x+1")  
+    #print("3. (-x*x)-2*x+1")  
+    #print("3. (x-3)*(x-3)-4") 
+    print("3. 4*(x-1)*(x-1)*(x-2)+0.5")
     #print("3. (x-9)*(x*x-1)*(x+1)+6")
-    print("4. (x-2)*(x-4)*(x-1)+7")
+    print("4. (x-2)*(x-4)*(x-1)")
+
 
 
 def intialization_method():
@@ -47,9 +50,11 @@ def select_function(function_number, x):
         f = x * x * x - 2
     elif function_number == 3:
     #f = (x - 9) * (x * x - 1) * (x + 1) + 6
-        f=(-x*x)-2*x+1
+    #f=(-x*x)-2*x+1
+    #f = (x - 3) * (x - 3) - 4
+        f = 4 * (x - 1) * (x - 1) * (x - 2) + 1/2
     else:
-        f = (x - 2) * (x - 4) * (x - 1) + 7
+        f = (x - 2) * (x - 4) * (x - 1)
     
     return f
 
@@ -63,9 +68,11 @@ def get_label(function_number):
         return "x*x*x-2"
     elif function_number == 3:
     #return "(x-9)*(x*x-1)*(x+1)+6"
-        return "(-x*x)-2*x+1"
+    #return "(-x*x)-2*x+1"
+    #return "(x-3)*(x-3)-4"
+        return "4*(x-1)*(x-1)*(x-2)+0.5"
     else:
-        return "(x-2)*(x-4)*(x-1)+7"
+        return "(x-2)*(x-4)*(x-1)"
 
 def make_interval(bnd1, bnd2):
     
@@ -419,12 +426,13 @@ if __name__ == '__main__':
        
         for a in range(-input_r, input_r+1):
             input_x = interval * a
-            input_range.append(input_x)
+            #input_range.append(input_x)
             x = FloatDPApproximation(input_x)
             counter = 0
             rootdisplay = 0
             
             if not (decide(f(x) == 0)):
+                input_range.append(input_x)
                 for j in range(len(all_method)):
                     if all_method[j] == 1:
                         t_start=time.process_time()
