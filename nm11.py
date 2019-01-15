d332915d7381b1fb00dd847081f4b95f7a290cab
Martyna <mikos.martyna@gmail.com>
2019-01-15 15:26:35 +0100

    Update nm11.py

--- nm11.py
+++ nm11.py
@@ -31,7 +31,7 @@ def intialization_method():
     print("2. General Newton Method with Initial estimator by using sign (NM_IE_SIGN)")
     print("3. General Newton Method with Initial estimator by using contractor (NM_IE_CON)")
     print("4. General Newton Method with Initial estimator by using sign method and second derivatives (NM_IE_SIGN_SECOND-DERIVATIVE)")
-    print("5. General Newton Method with Initial estimator by using contractor method and second derivatives (NM_IE_SIGN_SECOND-DERIVATIVE)")
+    print("5. General Newton Method with Initial estimator by using contractor method and second derivatives (NM_IE_CON_SECOND-DERIVATIVE)")
 
 
 def select_function(function_number, x):
